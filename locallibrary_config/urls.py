25f8b415e49d543c81102e1f15b46dd0345992a1
sofiam8 <mille7sx@dukes.jmu.edu>
2026-09-02 21:23:52 -0400

    final changes

---
+++ locallibrary_config/urls.py
@@ -0,0 +1,16 @@
+"""
+URL configuration for locallibrary_config project.
+"""
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='catalog/')),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
